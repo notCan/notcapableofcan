dd78ca9370417d01c67ddf91dca9ea4cca3c3f23
notCan <devecann@gmail.com>
2022-05-24 23:42:30 +0300

    lint

--- pages/pixelart.vue
+++ pages/pixelart.vue
@@ -1,15 +1,11 @@
 <template>
-  <div>
-
-  </div>
+  <div></div>
 </template>
 
 <script>
-  export default {
-    name:'PixelArtPage'
-  }
+export default {
+  name: 'PixelArtPage',
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
